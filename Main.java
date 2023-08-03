c0b6022f0dbb480ef2cede7f93208b22133a45d4
devendraswami <swamidevendra2025@gmail.com>
2023-08-03 18:46:39 +0530

    Updated

--- Main.java
+++ Main.java
@@ -1,129 +1,97 @@
 import java.util.Scanner;
 
-class Bank 
-{
-    // Intilized the    balance variable 
+class Bank { 
+
+    // Encapsulation Property is used here 
+
     private double balance;
 
-    public void checkBalance()
-    {
-        System.out.println("Your bank Balance is-  $" + balance);
+    public Bank(double balance) {
+        this.balance = balance;
     }
 
-
-    public void deposit(double amount)
-    {
-        balance += amount;
-        System.out.println("Your Amount has been depoisted Successfully, Now your Balnace is- $" + balance );
+    public void welcomeMsg() {
+        System.out.println("Hello! Welcome to our Bank");
     }
 
+    public double getBalance() {
+        return balance;
+    }
 
-    public void Withdrow(double amount)
-    {
-        if(balance < amount)
-        {
-            System.out.println("You Do not have enough balance, Sorry you can't Withdraw ");
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Your amount has been deposited successfully. Your new balance is: $" + balance);
+        } else {
+            System.out.println("You cannot deposit a negative or zero amount.");
         }
-        
-        else if(balance==0)
-        {
-            System.out.println("Your Account Balance is Zero, Sorry you can to withdraw ");
-        }
-        
-        else 
-        {
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("You cannot withdraw a negative or zero amount.");
+        } else if (amount > balance) {
+            System.out.println("You do not have enough balance. Withdrawal failed.");
+        } else {
             balance -= amount;
-            System.out.println("Your Amount has been Withdrawl Successfully, Now your Balance is-  $" + balance );
+            System.out.println("Your amount has been withdrawn successfully. Your new balance is: $" + balance);
         }
-       
     }
-     
+}
+ 
+
+// OOPs  concept inheritance is used here
+class SBI extends Bank {
+    public SBI(double balance) {
+        super(balance);
+    }
     
+
+    // OOps concept PolyMorphism used here (Run time polyMorphism )
+    @Override
+    public void welcomeMsg() {
+        System.out.println("Hello! Welcome to SBI Bank");
+    }
 }
 
+class Main {
+    public static void main(String[] args) {
+        SBI bank = new SBI(0);
+        bank.welcomeMsg();
 
-
-
-
-
-
-class Main 
-{
-    public static void main(String[] args) 
-    {
-        Bank bank = new Bank();
-        System.out.println("Hello, Welcome to our Bank App! ");
-       
-        
         Scanner scan = new Scanner(System.in);
-      
-        do 
-        { 
-            // Asking for What coustemer Wants 
-            
-            System.out.println("What would you like to do ? ");
-            System.out.println("1) Check Balance  ");
-            System.out.println("2)  Withdrow Cash ");
-            System.out.println("3) Depoist ");
-            System.out.println("4) Exit App ");
+
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("1) Check Balance");
+            System.out.println("2) Withdraw Cash");
+            System.out.println("3) Deposit");
+            System.out.println("4) Exit App");
 
             int choice = scan.nextInt();
-            if(choice == 1)
-            {
-               bank.checkBalance();
-            }
+            double amount;
 
-            else if(choice == 2 )
-            { 
-                double amount = 0;
-                System.out.println("Entre the amount you want to withdrow ");
-                amount = scan.nextDouble();
-
-                if(amount<=0) 
-                {
-                    System.out.println("You can't withdraw negative or Zero Amount ");
-                }
-
-                else 
-                {
-                   
-                    bank.Withdrow(amount);
-
-                }
-              
-            }
-
-            else if(choice ==3)
-            {
-                double amount =0;
-                System.out.println("Entre the amount you want to deposit ");
-                amount = scan.nextDouble();
-
-                if(amount <=0)
-                {
-                    System.out.println("YOu can't Deposit negative or Zero Amount ");
-                }
-
-                else{
+            switch (choice) {
+                case 1:
+                    System.out.println("Your bank balance is: $" + bank.getBalance());
+                    break;
+                case 2:
+                    System.out.println("Enter the amount you want to withdraw:");
+                    amount = scan.nextDouble();
+                    bank.withdraw(amount);
+                    break;
+                case 3:
+                    System.out.println("Enter the amount you want to deposit:");
+                    amount = scan.nextDouble();
                     bank.deposit(amount);
-                }
-                
+                    break;
+                case 4:
+                    System.out.println("Thank you for visiting our Bank Application");
+                    System.exit(0);
+                default:
+                    System.out.println("Enter a valid option between 1-4");
             }
 
-            else if(choice == 4)
-            {
-                System.out.println("ThankYOu for visiting our Bank Application ");
-                System.exit(choice);
-            }
-
-            else
-            {
-                System.out.println("Entre the right index between 1-4 ");
-            }
-    
-        }
-            while(true);
-       
-
+        } while (true);
     }
 }
